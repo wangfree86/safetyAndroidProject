9a92739e9f47e77c29e5c71e0ed25abb5897e261
wangfree86 <wangfree86@gmail.com>
2019-12-21 20:58:19 +0800

    Initial commit

---
+++ app/src/main/java/activity/ne/wk/com/safetyandroidproject/MainActivity.kt
@@ -0,0 +1,12 @@
+package activity.ne.wk.com.safetyandroidproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

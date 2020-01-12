df1358ec9f18f13cbd235d69acb76dc0893b5d1a
wangfree86 <wangfree86@gmail.com>
2020-01-12 21:57:14 +0800

    init

--- app/src/main/java/activity/ne/wk/com/safetyandroidproject/MainActivity.kt
+++ app/src/main/java/activity/ne/wk/com/safetyandroidproject/MainActivity.kt
@@ -2,7 +2,7 @@ package activity.ne.wk.com.safetyandroidproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+//111
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
